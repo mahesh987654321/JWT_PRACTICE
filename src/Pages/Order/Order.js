0fae4d2c957fc7755d46cc7c8a7ac0286b10ec4e
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-06-25 10:27:51 +0600

    specificemail

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -1,7 +1,27 @@
 import React from "react";
-
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { async } from "@firebase/util";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 const Order = () => {
-  return <div>order</div>;
+  const [order, setOrder] = useState([]);
+  const [user] = useAuthState(auth);
+  useEffect(() => {
+    const getOrder = async () => {
+      const email = user.email;
+      const url = `http://localhost:5000/order?email=${email}`;
+      const { data } = await axios.get(url);
+      setOrder(data);
+    };
+    getOrder();
+  }, [user]);
+  return (
+    <div>
+      <p>Order is {order?.length}</p>
+    </div>
+  );
 };
 
 export default Order;
