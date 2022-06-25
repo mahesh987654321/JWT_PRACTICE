666f9440b6ed1db823eda619d9aa6efae580fdf2
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-06-25 09:20:00 +0600

    axios

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,13 +1,84 @@
+import axios from "axios";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useParams } from "react-router-dom";
+import auth from "../../../firebase.init";
 import useServiceDetails from "../../../hooks/useServiceDetails";
 const Checkout = () => {
   const { serviceId } = useParams();
   const [service] = useServiceDetails(serviceId);
-
+  const [user] = useAuthState(auth);
+  const handlePlaceOrder = (event) => {
+    event.preventDefault();
+    const order = {
+      email: user.email,
+      service: service.name,
+      serviceId: serviceId,
+      address: event.target.address.value,
+      phone: event.target.phone.value,
+    };
+    axios.post("http://localhost:5000/order", order).then((res) => {
+      const { data } = res;
+      if (data.insertedId) {
+        alert("successful");
+      }
+    });
+  };
   return (
-    <div>
-      <h2>Please Book {service.name}</h2>
+    <div className="w-50 mx-auto">
+      <h2>Please Order: {service.name}</h2>
+      <form onSubmit={handlePlaceOrder}>
+        <input
+          className="w-100 mb-2"
+          type="text"
+          value={user?.displayName}
+          name="name"
+          placeholder="name"
+          required
+          readOnly
+          disabled
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="email"
+          value={user?.email}
+          name="email"
+          placeholder="email"
+          required
+          readOnly
+          disabled
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="text"
+          value={service.name}
+          name="service"
+          placeholder="service"
+          required
+          readOnly
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="text"
+          name="address"
+          placeholder="address"
+          autoComplete="off"
+          required
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="text"
+          name="phone"
+          placeholder="phone"
+          required
+        />
+        <br />
+        <input className="btn btn-primary" type="submit" value="Place Order" />
+      </form>
     </div>
   );
 };
